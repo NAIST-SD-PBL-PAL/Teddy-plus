79f07ec0bcff10b8794ef1adde617f6c936c86b3
Ayanoikegami <onaya91119@gmail.com>
2021-02-05 15:40:03 +0900

    add options for specifing interval

--- detection-mode/AutoRun.sh
+++ detection-mode/AutoRun.sh
@@ -1,24 +1,58 @@
 #!/bin/bash
+echo $#
+
+
 if [ "$1" != "" ]; then
     echo 'GitHub Repo To Clone =' $1
+    cloneRepo=$1
 else
     echo "parameter 1 is empty"
 fi
 if [ "$2" != "" ]; then
+    location=$2
 	echo 'Output Result Location =' $2
 else
     echo "parameter 2 is empty"
 fi
 if [ "$3" != "" ]; then
 	echo 'Elasticsearch index name =' $3
+    index=$3
 else
     echo "parameter 3 is empty"
 fi
-git clone $1 
+git clone $cloneRepo
 cd */.
+for OPT in "$@"
+do
+    case $OPT in
+        -a) echo "-a is " $2
+            date=$2
+            after=" --after ${date} " 
+            shift;;
+        -b) echo "-b is " $2
+            date=$2
+            before=" --before ${date}"
+            shift;;
+        *) shift;;
+    esac
+done
+
+
 #ls -l
 gitclonedhead=$(git symbolic-ref --short HEAD)
-commit=$(git log --oneline --pretty=format:"%h;")
+commitCommand="git log --oneline --pretty=format:\"%h;\""
+if [ "$after" ]; then
+    commitCommand=$commitCommand$after
+else
+    echo "no option for after"
+fi
+if [ "$before" ]; then
+    commitCommand=$commitCommand$before
+else
+    echo "no option for before"
+fi
+commit=${commitCommand}
+echo $commitCommand
 commitarray=(${commit//;/})
 commitNo=0
 for ((i=${#commitarray[@]};i>0;i--))
@@ -28,7 +62,7 @@ echo "Commit#$i ID:${commitarray[$i]} >> Starting search"
 git checkout ${commitarray[$i]}
 # Calling shell script that runs Siamese.jar, perform indexing and searching
 #=====================================
-./../CallSiamese.sh $PWD ./../../python-idioms $2 $3 ${commitarray[$i]} $commitNo
+./../CallSiamese.sh $PWD ./../../python-idioms $location $index ${commitarray[$i]} $commitNo
 #=====================================
 
   ## Uncomment/Comment for manual/auto iteration of commit versions
@@ -43,4 +77,4 @@ rm -r */
 ## echo or run tatts scrip
 
 # Calling shell script that runs bokeh visualization
-./CallBokeh.sh $2
+./CallBokeh.sh $location
