2929cb254b8b0cb0bedec5744034bcf53310424c
gqlo <escita@pm.me>
2021-02-19 15:55:48 +0900

    clean up script

--- detection-mode/AutoRun.sh
+++ detection-mode/AutoRun.sh
@@ -3,22 +3,23 @@ echo $#
 
 if [ "$1" != "" ]; then
     echo "[TeddyPlus] GitHub Repo To Clone =" $1
-    cloneRepo=$1
+    export cloneRepo=$1
 else
     echo "[TeddyPlus:ERROR] Parameter 1 is empty"
 fi
 if [ "$2" != "" ]; then
-    location=$2
+    export location=$2
 	echo "[TeddyPlus] Output Result Location =" $2
 else
     echo "[TeddyPlus:ERROR] parameter 2 is empty"
 fi
 if [ "$3" != "" ]; then
 	echo "[TeddyPlus] Repository name =" $3
-    index=$3
+    export index=$3
 else
     echo "[TeddyPlus:ERROR] Parameter 3 is empty"
 fi
+
 git clone $cloneRepo && cd "$(basename "$_" .git)"
 echo $(pwd)
 for OPT in "$@"
@@ -36,8 +37,6 @@ do
     esac
 done
 
-
-#ls -l
 gitclonedhead=$(git symbolic-ref --short HEAD)
 commitCommand="git log --oneline --pretty=format:\"%h;\""
 if [ "$after" ]; then
@@ -46,17 +45,20 @@ fi
 if [ "$before" ]; then
     commitCommand=$commitCommand$before
 fi
+
 echo $commitCommand
 commit=$($commitCommand)
 echo $commit
 commitarray=(${commit//;/})
 echo $commitarrayi
 commit_count=${#commitarray[@]}
+
 if [ $commit_count == "0" ]; then
     echo "There are no commits."
 else
     echo "There are some commits."
 fi
+
 commitNo=0
 for ((i=${#commitarray[@]};i>0;i--))
 do
@@ -65,21 +67,14 @@ echo "[TeddyPlus] Commit#$i ID:${commitarray[$i]} >> Starting search"
 git checkout ${commitarray[$i]}
 # Calling shell script that runs Siamese.jar, perform indexing and searching
 #=====================================
-./../CallSiamese.sh $PWD ./../../python-idioms $location $index ${commitarray[$i]} $commitNo
+/home/teddy/client/CallSiamese.sh $PWD /home/teddy/client/python-idioms $location $index ${commitarray[$i]} $commitNo
 #=====================================
 
-  ## Uncomment/Comment for manual/auto iteration of commit versions
-  ##echo "Press any Key to Continue to the next commit"
-  ##read -n 1
 git checkout $gitclonedhead
 ((commitNo=commitNo+1))
 done
-## Deleting the repo folder after iteration is completed
-
-#cd ..
-#rm -r */
 
 ## echo or run tatts scrip
 
 # Calling shell script that runs bokeh visualization
-./CallBokeh.sh $2
+/home/teddy/client/CallBokeh.sh $location
