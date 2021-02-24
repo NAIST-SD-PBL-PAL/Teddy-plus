53cda7dc62512c8e13e7eda96b035e77c200efd1
gqlo <escita@pm.me>
2021-02-24 23:19:51 +0900

    use absolute path

--- detection-mode/CallBokeh.sh
+++ detection-mode/CallBokeh.sh
@@ -1,2 +1,2 @@
 # $1 is the location of output CSV files from Siamese
-python3 bokeh_visualization_teddyPlus.py $1 ./idiomatic.csv ./nonidiomatic.csv
+python3 /home/teddy/client/bokeh_visualization_teddyPlus.py $location ./idiomatic.csv ./nonidiomatic.csv
